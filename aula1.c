a2737263e5482d845a6f9f8c1ec7a377b10cd385
João Pedro Moreto Lourenção <48949350+Joao-Pedro-ML@users.noreply.github.com>
2020-03-04 09:10:46 -0300

    Update and rename main.c to aula1.c

---
+++ aula1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubblesort(int *V, int tam){
+  int bool = 0;
+  do{
+    bool = 0;
+    for(int i = 0;i<tam;i++){
+      if(V[i] > V[i+1]){
+        int a = V[i];
+        V[i] = V[i+1];
+        V[i+1] = a;
+        bool = 1;
+      }
+    }
+  }while (bool != 0);
+
+}
+
+void exibeVetor(int *V, int tam){
+  for(size_t i = 0; i<10; i++){
+    printf("%i\n", V[i]);
+  }
+}
+
+int main(int argc, char const *argv[]){
+
+  int vetor[10] = {5,1,76,24,-9,80,3,12,-87,8};
+  exibeVetor(vetor, 10);
+  bubblesort(vetor, 10);
+  printf("\n-----------------\n");
+  exibeVetor(vetor, 10);
+
+  return 0;
+}
