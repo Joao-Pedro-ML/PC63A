5349f8fa53cdd13ebd6909740a7adce97fd4cea7
João Pedro Moreto Lourenção <48949350+Joao-Pedro-ML@users.noreply.github.com>
2020-03-06 09:07:56 -0300

    Update aula1.c

--- aula1.c
+++ aula1.c
@@ -1,33 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void bubbleSort(int *V, int tam){
+void bubblesort(int *V, int tam){
+
   int bool = 0;
   do{
     bool = 0;
-    for(int i = 0 ; i<tam-1 ; i++){
+    for(int i = 0;i<tam-1;i++){
       if(V[i] > V[i+1]){
         int a = V[i];
         V[i] = V[i+1];
         V[i+1] = a;
         bool = 1;
-      }
-    }
+      }//if
+    }//for
+    tam--;
   }while (bool != 0);
 
 }
 
 void exibeVetor(int *V, int tam){
-  for(size_t i = 0; i<10; i++){
+
+  for(size_t i = 0; i<tam; i++){
     printf("%i\n", V[i]);
   }
+
 }
 
 int main(int argc, char const *argv[]){
 
-  int vetor[10] = {5,1,76,24,-9,80,3,12,-87,8};
+  int vetor[15] = {5,1,76,24,-9,80,3,12,-87,8};
   exibeVetor(vetor, 10);
-  bubbleSort(vetor, 10);
+  bubblesort(vetor, 10);
   printf("\n-----------------\n");
   exibeVetor(vetor, 10);
 
