d3388166739103c86b12d337dc9aa0298baf50f3
João Pedro Moreto Lourenção <48949350+Joao-Pedro-ML@users.noreply.github.com>
2020-03-06 14:11:49 -0300

    Update SelectionSort.c

--- SelectionSort.c
+++ SelectionSort.c
@@ -2,39 +2,42 @@
 #include <stdlib.h>
 
 void SelectionSort(int *V, int tam){
-
-  int a;
-  int x = 0;
-  int menor = 0;
-
-  do{
-    for(int i = x; i<tam-1; i++){
-      if(V[i]>V[menor]){
-        menor = i;
-      }//if
-      a = V[menor];
-      V[menor] = V[x];
-      V[x] = a; 
-      x++;
-    }//for
-  }while(x != tam);
-}//void
+    
+    int aux;
+    int temp;
+    int menor;
+    int troca;
+    
+    for(aux = 0; aux<tam-1; aux++){
+        menor = aux;
+        for (temp=aux+1; temp < tam; temp++){
+        if (V[temp] < V[menor]){
+                menor = temp;
+            }
+        }
+        if (menor != aux){
+            troca = V[aux];
+            V[aux]   = V[menor];
+            V[menor] = troca;
+        }
+    }
+}
 
 void exibeVetor(int *V, int tam){
-
-  for(size_t i = 0; i<tam; i++){
-    printf("%i\n", V[i]);
-  }
-
+    
+    for(size_t i = 0; i<tam; i++){
+        printf("%i\n", V[i]);
+    }
+    
 }
 
 int main(int argc, char const *argv[]){
-
-  int vetor[10] = {10,-2,21,32,7,0,9,6,46,-5};
-  exibeVetor(vetor, 10);
-  SelectionSort(vetor, 10);
-  printf("\n-----------------\n");
-  exibeVetor(vetor, 10);
-
-  return 0;
+    
+    int vetor[10] = {10,-2,21,32,7,0,9,6,46,-5};
+    exibeVetor(vetor, 10);
+    SelectionSort(vetor, 10);
+    printf("\n-----------------\n");
+    exibeVetor(vetor, 10);
+    
+    return 0;
 }
