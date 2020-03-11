f72438d2b36c9da08d6db6bea29343e1d62596db
João Pedro Moreto Lourenção <48949350+Joao-Pedro-ML@users.noreply.github.com>
2020-03-11 16:14:12 -0300

    Update InsertionSort.c

--- InsertionSort.c
+++ InsertionSort.c
@@ -1,47 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /**
-  *\brief
-*/
+ *\brief Realiza o insertionSort
+ *
+ *\param V ponteiro para o vetor a ser exibido
+ *\param tam tamanho do vetor
+ *
+ *
+ */
 void InsertionSort(int *V, int tam){
-  int aux;
-  int j
-  for(int i = 1; i < tam; i++){
-    aux = V[i];
-
-      for(j = i; j > 0 && aux < V[j-1]; j--){
-
-          V[j] = V[j-1];
-
-      }//for
-      V[j] = aux;
-
-  }//for
+    int aux;
+    int j;
+    for(int i = 1; i < tam; i++){
+        aux = V[i];
+        
+        for(j = i; j > 0 && aux < V[j-1]; j--){
+            
+            V[j] = V[j-1];
+            
+        }//for
+        V[j] = aux;
+        
+    }//for
 }//InserctionSort
 
 /**
-  *\brief Exibe um vetor
-  *
-  *\param V ponteiro para o vetor a ser exibido
-  *\param tam para o tamanho do vetor
-*/
+ *\brief Exibe um vetor
+ *
+ *\param V ponteiro para o vetor a ser exibido
+ *\param tam para o tamanho do vetor
+ */
 
 void exibeVetor(int *V, int tam){
-
+    
     for(size_t i = 0; i<tam; i++){
         printf("%i\n", V[i]);
     }
-
+    
 }
 
 int main(int argc, char const *argv[]){
-
-  int vetor[10] = {11,20,-9,2,-5,32,4,12,95,6};
-  exibeVetor(vetor, 10);
-  InsertionSort(vetor, 10);
-  printf("\n-----------------\n");
-  exibeVetor(vetor, 10);
-
-  return 0;
+    
+    int vetor[15];
+    for(int i = 0; i<15; i++){
+        vetor[i] = rand()%100;
+    }//for
+    exibeVetor(vetor, 15);
+    InsertionSort(vetor, 15);
+    printf("\n-----------------\n");
+    exibeVetor(vetor, 15);
+    
+    return 0;
 }
