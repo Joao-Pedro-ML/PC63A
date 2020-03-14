31b231ab51cb32f76ec4f8ce085a3ba7ca3c4297
João Pedro Moreto Lourenção <jlourencao@alunos.utfpr.edu.br>
2020-03-14 01:22:17 -0300

    Update SelectionSort.c

--- SelectionSort.c
+++ SelectionSort.c
@@ -1,43 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-void SelectionSort(int *V, int tam){
-    
-    int aux;
-    int temp;
-    int menor;
-    int troca;
-    
-    for(aux = 0; aux<tam-1; aux++){
-        menor = aux;
-        for (temp=aux+1; temp < tam; temp++){
-        if (V[temp] < V[menor]){
-                menor = temp;
-            }
-        }
-        if (menor != aux){
-            troca = V[aux];
-            V[aux]   = V[menor];
-            V[menor] = troca;
-        }
-    }
-}
+#include <time.h>
 
 void exibeVetor(int *V, int tam){
-    
-    for(size_t i = 0; i<tam; i++){
-        printf("%i\n", V[i]);
-    }
-    
+
+  for(size_t i = 0; i<tam; i++){
+      printf("%i\n", V[i]);
+  }
+
 }
 
+int *vetor(int tam){
+    int *tmp=NULL;
+    tmp=(int*)malloc(tam*sizeof(int));
+    for(int i=0;i<tam;i++){
+        tmp[i]=rand()%100;
+    }//for
+    return tmp;
+}//func vetor
+
+/**
+ * \brief Intercala os subvetores mantendo a ordenação
+ *
+ * \param vetor ponteiro para o vetor a ser intercalado
+ * \param inicio indice inicial do subvetor1
+ * \param meio indice final do subvetor1
+ * \param fim indice final do subvetor2
+ *
+ * 1 - Aloca o vetor temporario
+ * 2 - Copia os valores para o vetor
+ *     temporário intercalando
+ * 3 - Volta os valores para o vetor original
+ * 4 - Libera memória do vetor temporario
+ */
+
+void merge(int *vetor, int inicio, int meio, int fim){
+
+  meio = ((fim+inicio)/2);
+  int *aux = (int *)malloc(fim*sizeof(int));
+  for(int i = 0; i<fim; i++){
+    if(inicio >= fim/2){
+      aux[i] = vetor[meio];
+      meio++;
+    }//if
+    if(){
+      
+    }//if
+  }//for
+
+}//merge
+
+/**
+ * \brief Ordena o vetor usando Mergesort
+ *
+ * \param vetor ponteiro para o vetor a ser ordenado
+ * \param inicio indice inicial do vetor
+ * \param fim indice final do vetor
+ *
+ * 1 - Testa o caso base
+ * 2 - Faz as 2 chamadas recursivas
+ * 3 - Chama a função merge para intercalar os 2 subvetores
+ */
+
+void mergeSort(int *vetor, int inicio, int fim){
+
+  if(inicio == fim){
+    
+  }
+
+}//mergeSort
+
 int main(int argc, char const *argv[]){
-    
-    int vetor[10] = {10,-2,21,32,7,0,9,6,46,-5};
-    exibeVetor(vetor, 10);
-    SelectionSort(vetor, 10);
-    printf("\n-----------------\n");
-    exibeVetor(vetor, 10);
-    
-    return 0;
+
+  int *V = NULL;
+  V = vetor(10);
+  exibeVetor(V, 10);
+
+  return 0;
 }
