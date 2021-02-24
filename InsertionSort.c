4ef22395be453764e1e4648471ef9f0770d8aa84
João Pedro Moreto Lourenção <jlourencao@alunos.utfpr.edu.br>
2021-02-24 19:56:55 -0300

    Update InsertionSort.c

--- InsertionSort.c
+++ InsertionSort.c
@@ -2,55 +2,41 @@
 #include <stdlib.h>
 #include <time.h>
 
-/**
- *\brief Realiza o insertionSort
- *
- *\param V ponteiro para o vetor a ser exibido
- *\param tam tamanho do vetor
- *
- *
- */
-void InsertionSort(int *V, int tam){
-    int aux;
-    int j;
-    for(int i = 1; i < tam; i++){
-        aux = V[i];
-        
-        for(j = i; j > 0 && aux < V[j-1]; j--){
-            
-            V[j] = V[j-1];
-            
-        }//for
-        V[j] = aux;
-        
-    }//for
-}//InserctionSort
-
-/**
- *\brief Exibe um vetor
- *
- *\param V ponteiro para o vetor a ser exibido
- *\param tam para o tamanho do vetor
- */
-
 void exibeVetor(int *V, int tam){
-    
-    for(size_t i = 0; i<tam; i++){
-        printf("%i\n", V[i]);
-    }
-    
+
+  for(size_t i = 0; i<tam; i++){
+      printf("%i\n", V[i]);
+  }
+
 }
+
+void SelectionSort(int* V, int a){
+    int menor, troca;
+    for (int i = 0; i<a; i++) {
+        menor = i;
+        for (int j=i+1; j<a; j++) {
+            if(V[j]<V[menor]){
+                menor = j;
+            }//if
+        }//for
+        if (i!=menor) {
+            troca=V[i];
+            V[i] = V[menor];
+            V[menor]= troca;
+        }//if
+    }//for
+}//selectionsort
 
 int main(int argc, char const *argv[]){
     
-    int vetor[15];
-    for(int i = 0; i<15; i++){
+    int vetor[10];
+    for(int i = 0; i<10; i++){
         vetor[i] = rand()%100;
     }//for
-    exibeVetor(vetor, 15);
-    InsertionSort(vetor, 15);
+    exibeVetor(vetor, 10);
+    SelectionSort(vetor, 10);
     printf("\n-----------------\n");
-    exibeVetor(vetor, 15);
+    exibeVetor(vetor, 10);
     
     return 0;
 }
